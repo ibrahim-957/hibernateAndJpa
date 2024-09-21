9843690ec9b3bca06f4f3a33b1dd3b4c2a264b2c
ibrahim-957 <n.naghiyev.ibrahim@gmail.com>
2024-09-21 16:09:46 +0400

    first commit

---
+++ src/main/java/com/javacourse/project/hibernateAndJpa/DataAccess/HibernateCityDal.java
@@ -0,0 +1,55 @@
+package com.javacourse.project.hibernateAndJpa.DataAccess;
+
+import com.javacourse.project.hibernateAndJpa.Entities.City;
+import jakarta.persistence.EntityManager;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+@Repository
+public class HibernateCityDal implements ICityDal{
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public HibernateCityDal(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+    // AOP - aspect oriented programing
+    @Override
+    @Transactional
+    public List<City> getAll() {
+        Session session = entityManager.unwrap(Session.class);
+        List<City> cities = session.createQuery("from City",City.class).getResultList();
+        return cities;
+    }
+
+    @Override
+    public void add(City city) {
+        Session session = entityManager.unwrap(Session.class);
+        session.persist(city);
+    }
+
+    @Override
+    public void update(City city) {
+        Session session = entityManager.unwrap(Session.class);
+        session.merge(city);
+    }
+
+    @Override
+    public void delete(City city) {
+        Session session = entityManager.unwrap(Session.class);
+        City cityToDelete = session.get(City.class,city.getId());
+        session.delete(cityToDelete);
+    }
+
+    @Override
+    public City getById(int id) {
+        Session session = entityManager.unwrap(Session.class);
+        City city = session.get(City.class, id);
+        return city;
+    }
+
+}
